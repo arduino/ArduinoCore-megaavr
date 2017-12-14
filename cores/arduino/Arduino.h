233acde632fcd446d144da0190790deddf190c8e
Thomaz, Martin <Martin.Thomaz@atmel.com>
2017-12-14 14:47:26 +0100

    Fixed bug, portToPortStruct() was checking agains NOT_A_PORT (max value), but should check against MAX port numbers. Even with NOT_A_PORT being 255 (previouse 0), it could return an invalid address instead of NULL.

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -61,14 +61,55 @@ extern uint32_t F_CPU_CORRECTED;
 #define interrupts() sei()
 #define noInterrupts() cli()
 
-uint16_t clockCyclesPerMicrosecond(uint32_t clk);
-uint16_t clockCyclesToMicroseconds(uint16_t cycles, uint32_t clk);
-uint32_t microsecondsToClockCycles(uint16_t cycles, uint32_t clk);
-
 // avr-libc defines _NOP() since 1.6.2
 #ifndef _NOP
 #define _NOP() do { __asm__ volatile ("nop"); } while (0)
 #endif
+
+typedef unsigned int word;
+
+#define bit(b) (1UL << (b))
+
+typedef bool boolean;
+typedef uint8_t byte;
+
+extern uint32_t F_CPU_CORRECTED;
+
+void init(void);
+void initVariant(void);
+
+uint16_t clockCyclesPerMicrosecond(uint32_t clk);
+uint16_t clockCyclesToMicroseconds(uint16_t cycles, uint32_t clk);
+uint32_t microsecondsToClockCycles(uint16_t cycles, uint32_t clk);
+
+uint16_t clockCyclesPerMicrosecondOrig();
+uint16_t clockCyclesToMicrosecondsOrig(uint16_t cycles);
+uint32_t microsecondsToClockCyclesOrig(uint16_t cycles);
+
+int atexit(void (*func)()) __attribute__((weak));
+
+void pinMode(uint8_t, uint8_t);
+void digitalWrite(uint8_t, uint8_t);
+int digitalRead(uint8_t);
+int analogRead(uint8_t);
+void analogReference(uint8_t mode);
+void analogWrite(uint8_t, int);
+
+unsigned long millis(void);
+unsigned long micros(void);
+void delay(unsigned long);
+void delayMicroseconds(unsigned int us);
+unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout);
+unsigned long pulseInLong(uint8_t pin, uint8_t state, unsigned long timeout);
+
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
+uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder);
+
+void attachInterrupt(uint8_t, void (*)(void), int mode);
+void detachInterrupt(uint8_t);
+
+void setup(void);
+void loop(void);
 
 // Get the bit location within the hardware port of the given virtual pin.
 // This comes from the pins_*.c file for the active board configuration.
@@ -85,19 +126,6 @@ extern const uint8_t PROGMEM digital_pin_to_interrupt[];
 //
 // These perform slightly better as macros compared to inline functions
 //
-#define digitalPinToPort(pin) ( (pin < NUM_TOTAL_PINS) ? pgm_read_byte(digital_pin_to_port + pin) : NOT_A_PIN )
-#define digitalPinToBitPosition(pin) ( (pin < NUM_TOTAL_PINS) ? pgm_read_byte(digital_pin_to_bit_position + pin) : NOT_A_PIN )
-#define analogPinToBitPosition(pin) ( (pin < NUM_ANALOG_INPUTS) ? pgm_read_byte(digital_pin_to_bit_position + pin + 14) : NOT_A_PIN )
-#define digitalPinToBitMask(pin) ( (pin < NUM_TOTAL_PINS) ? (1 << digitalPinToBitPosition(pin)) : NOT_A_PIN )
-#define analogPinToBitMask(pin) ( (pin < NUM_ANALOG_INPUTS) ? (1 << analogPinToBitPosition(pin)) : NOT_A_PIN )
-#define digitalPinToTimer(pin) ( (pin < NUM_TOTAL_PINS) ? pgm_read_byte(digital_pin_to_timer + pin) : NOT_ON_TIMER )
-
-#define portToPortStruct(port) ( (port < NUMBER_OF_PORTS) ? ((PORT_t *)&PORTA + port) : NULL)
-#define digitalPinToPortStruct(pin) ( (pin < NUM_TOTAL_PINS) ? ((PORT_t *)&PORTA + digitalPinToPort(pin)) : NULL)
-#define getPINnCTRLregister(port_addr, bit_pos) ( ((port_addr != NULL) && (bit_pos < NOT_A_PIN)) ? ((uint8_t *)&(port_addr->PIN0CTRL) + bit_pos) : NULL )
-#define digitalPinToInterrupt(p) ( pgm_read_byte(digital_pin_to_interrupt + p) )
-#define portPinToInterrupt(port, bit_pos) ( pgm_read_byte(port_interrupt_offset + port) + bit_pos )
-
 #define NOT_A_PIN 255
 #define NOT_A_PORT 255
 #define NOT_AN_INTERRUPT 255
@@ -108,7 +136,7 @@ extern const uint8_t PROGMEM digital_pin_to_interrupt[];
 #define PD 3
 #define PE 4
 #define PF 5
-#define NUMBER_OF_PORTS 6
+#define NUM_TOTAL_PORTS 6
 
 #define NOT_ON_TIMER 0
 #define TIMERA0 1
@@ -116,6 +144,19 @@ extern const uint8_t PROGMEM digital_pin_to_interrupt[];
 #define TIMERB1 3
 #define TIMERB2 4
 #define TIMERB3 5
+
+#define digitalPinToPort(pin) ( (pin < NUM_TOTAL_PINS) ? pgm_read_byte(digital_pin_to_port + pin) : NOT_A_PIN )
+#define digitalPinToBitPosition(pin) ( (pin < NUM_TOTAL_PINS) ? pgm_read_byte(digital_pin_to_bit_position + pin) : NOT_A_PIN )
+#define analogPinToBitPosition(pin) ( (pin < NUM_ANALOG_INPUTS) ? pgm_read_byte(digital_pin_to_bit_position + pin + 14) : NOT_A_PIN )
+#define digitalPinToBitMask(pin) ( (pin < NUM_TOTAL_PINS) ? (1 << digitalPinToBitPosition(pin)) : NOT_A_PIN )
+#define analogPinToBitMask(pin) ( (pin < NUM_ANALOG_INPUTS) ? (1 << analogPinToBitPosition(pin)) : NOT_A_PIN )
+#define digitalPinToTimer(pin) ( (pin < NUM_TOTAL_PINS) ? pgm_read_byte(digital_pin_to_timer + pin) : NOT_ON_TIMER )
+
+#define portToPortStruct(port) ( (port < NUM_TOTAL_PORTS) ? ((PORT_t *)&PORTA + port) : NULL)
+#define digitalPinToPortStruct(pin) ( (pin < NUM_TOTAL_PINS) ? ((PORT_t *)&PORTA + digitalPinToPort(pin)) : NULL)
+#define getPINnCTRLregister(port, bit_pos) ( ((port != NULL) && (bit_pos < NOT_A_PIN)) ? ((uint8_t *)&(port->PIN0CTRL) + bit_pos) : NULL )
+#define digitalPinToInterrupt(p) ( pgm_read_byte(digital_pin_to_interrupt + p) )
+#define portPinToInterrupt(port, bit_pos) ( pgm_read_byte(port_interrupt_offset + port) + bit_pos )
 
 #ifdef __cplusplus
 } // extern "C"
